379e3769512a1ec831730052929f3895b8f5a407
RodterRose <128921415+RodterRose@users.noreply.github.com>
2023-03-28 00:08:29 +0530

    Add files via upload

---
+++ Roster_loading/assets/scripts/config.js
@@ -0,0 +1,33 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "localhost:30120";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: " Join our community! discord.gg/Q92nMpQNuN ", icon: "https://cdn.discordapp.com/attachments/1089219335693803611/1089978179155922964/d.png", link: "https://discord.gg/Q92nMpQNuN"},
+    {name: "saltychat", label: "Saltychat", description: "4YOU", icon: "https://cdn.discordapp.com/attachments/1089219335693803611/1089977554275938304/y.png", link: "https://discord.gg/Q92nMpQNuN"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "Sonny Black", description: "Inhaber", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+    {name: "Ragnarr", description: "Inhaber", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "Bitcoindieb", description: "Stv.Inhaber", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "TimmyTrash", description: "Stv.Inhaber", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "King.exe", description: "Projektleitung", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "Ketamin", description: "Stv.Projektleitung", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "Mukhtar", description: "Manager", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+     {name: "Prometheuz", description: "Head Developer", color: "#6A00E4", image: "https://media.discordapp.net/attachments/1023603350350073906/1052655500925358160/logo_discord.gif"},
+
+];
+// Categories
+Config.Categories = [
+    {label: "Social Media", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
